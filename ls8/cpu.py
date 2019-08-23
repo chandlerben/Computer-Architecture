e093eef76865cd449840491e4dfceaa4219d6882
benjchand <benchand@gmail.com>
2019-08-23 09:04:48 -0700

    Working on Sprint

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,101 +3,6 @@
 import sys
 
 
-# class CPU:
-#     """Main CPU class."""
-
-#     def __init__(self):
-#         """Construct a new CPU."""
-#         self.ram = [0] * 256
-#         self.reg = [0] * 8
-#         self.pc = 0
-
-#     def load(self, file):
-#         """Load a program into memory."""
-
-#         address = 0
-
-#         # For now, we've just hardcoded a program:
-
-#         with open(file, 'r') as f:
-#             for line in f:
-#                 if line.startswith('#') or line.startswith('\n'):
-#                     continue
-#                 else:
-#                     instruction = line.split(' ')[0]
-#                     self.ram[address] = int(instruction, 2)
-#                     address += 1
-
-#     def ram_read(self, mar):
-#         return self.ram[mar]
-
-#     def ram_write(self, mar, mdr):
-#         self.ram[mar] = mdr
-
-#     def alu(self, op, reg_a, reg_b):
-#         """ALU operations."""
-
-#         if op == "ADD":
-#             self.reg[reg_a] += self.reg[reg_b]
-#         # elif op == "SUB": etc
-#         elif op == "MUL":
-#             self.reg[reg_a] += self.reg[reg_b]
-#         else:
-#             raise Exception("Unsupported ALU operation")
-
-#     def trace(self):
-#         """
-#         Handy function to print out the CPU state. You might want to call this
-#         from run() if you need help debugging.
-#         """
-
-#         print(f"TRACE: %02X | %02X %02X %02X |" % (
-#             self.pc,
-#             # self.fl,
-#             # self.ie,
-#             self.ram_read(self.pc),
-#             self.ram_read(self.pc + 1),
-#             self.ram_read(self.pc + 2)
-#         ), end='')
-
-#         for i in range(8):
-#             print(" %02X" % self.reg[i], end='')
-
-#         print()
-
-#     def run(self):
-#         """Run the CPU."""
-#         LDI = 0b10000010
-#         PRN = 0b01000111
-#         HLT = 0b00000001
-#         MUL = 0b10100010
-
-#         running = True
-
-#         while running:
-#             ir = self.ram_read(self.pc)
-
-#             if ir == HLT:
-#                 running = False
-
-#             if ir == PRN:
-#                 reg = self.ram_read(self.pc + 1)
-#                 print(self.reg[reg])
-#                 self.pc += 2
-
-#             if ir == LDI:
-#                 reg_a = self.ram_read(self.pc + 1)
-#                 reg_b = self.ram_read(self.pc + 2)
-#                 self.reg[reg_a] = reg_b
-#                 self.pc += 3
-
-#             if ir == MUL:
-#                 reg_a = self.ram_read(self.pc + 1)
-#                 reg_b = self.ram_read(self.pc + 2)
-#                 self.alu("MUL", reg_a, reg_b)
-#                 self.pc += 3
-
-
 HLT = 0b00000001
 PRN = 0b01000111
 LDI = 0b10000010
@@ -105,9 +10,10 @@ PUSH = 0b01000101
 POP = 0b01000110
 CALL = 0b01010000
 RET = 0b00010001
-
 MUL = 0b10100010
 ADD = 0b10100000
+CMP = 0b10100111
+JMP = 0b01010100
 
 
 # Main CPU class
@@ -118,8 +24,11 @@ class CPU:
         self.reg = [0] * 8
         self.pc = 0
         self.sp = 7  # stack pointer is always register 7
+        self.fl = [0] * 8
 
         self.dispatchtable = {
+            JMP: self.jmp,
+            CMP: self.CMP,
             MUL: self.mul,
             ADD: self.add,
             PRN: self.prn,
@@ -160,6 +69,17 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
 
+        if op == "CMP":
+            self.fl[7] = 0
+            self.fl[6] = 0
+            self.fl[5] = 0
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.fl[7] = 1
+            elif self.reg[reg_a] < self.reg[reg_b]:
+                self.fl[5] = 1
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.fl[6] = 1
+
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
 
@@ -181,6 +101,17 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+
+    # Handle CMP
+
+    def CMP(self, reg_a, reg_b):
+        self.alu("CMP", reg_a, reg_b)
+        self.pc += 3
+
+    # Handle JMP
+
+    def jmp(self, reg_a):
+        self.pc = self.reg[reg_a]
 
     # Handle MUL
 
